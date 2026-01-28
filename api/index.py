e70abdedd0994fe3be0d68ea1640edfa5b9e5200
Anujpy12345 <githubsetups@gmail.com>
2026-01-28 16:38:42 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -29,8 +29,8 @@ def is_user_joined(chat_id):
 def send_force_join(chat_id):
     markup = types.InlineKeyboardMarkup()
     markup.row(
-        types.InlineKeyboardButton("📢 Join Public", url=f"https://t.me/{PUBLIC_CHANNEL.strip('@')}"),
-        types.InlineKeyboardButton("🔐 Join Private", url="https://t.me/+mL09ZgHctA5hZDA9" + str(PRIVATE_CHANNEL_ID)[4:])
+        types.InlineKeyboardButton("📢 Join 1", url=f"https://t.me/{PUBLIC_CHANNEL.strip('@')}"),
+        types.InlineKeyboardButton("🔐 Join 2", url="https://t.me/+h1s1jG6XizAxMzQ1" + str(PRIVATE_CHANNEL_ID)[4:])
     )
     markup.add(types.InlineKeyboardButton("✅ JOINED", callback_data="check_join"))
     bot.send_message(chat_id, "🚫 To use this bot, please join both channels first:", reply_markup=markup)
